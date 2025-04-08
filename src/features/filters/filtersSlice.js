9c2e5050aa29f5a1ce90af7450b44687504d3d8f
Paphop Sawasdee <paphop_sawasdee@earthbrain.com>
2025-04-08 14:02:42 +0900

    Add filters reducer functions

--- src/features/filters/filtersSlice.js
+++ src/features/filters/filtersSlice.js
@@ -11,6 +11,31 @@ export default function filtersReducer(state = initialState, action) {
         status: action.payload,
       }
     }
+    case 'filters/colorFilterChanged': {
+      let { color, changeType } = action.payload
+      const { colors } = state
+
+      switch (changeType) {
+        case 'added': {
+          if (colors.includes(color)) {
+            return state
+          }
+
+          return {
+            ...state,
+            colors: colors.concat(color),
+          }
+        }
+        case 'removed': {
+          return {
+            ...state,
+            colors: colors.filter((existingColor) => existingColor !== color),
+          }
+        }
+        default:
+          return state
+      }
+    }
     default:
       return state
   }
