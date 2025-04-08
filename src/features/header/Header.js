257e501b422f780014ab700a20a19305765b138f
Paphop Sawasdee <paphop_sawasdee@earthbrain.com>
2025-04-08 15:30:52 +0900

    Connect redux store

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,8 +1,23 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
 export default function Header() {
   const [text, setText] = useState('')
+  const dispatch = useDispatch()
 
   const handleChange = (e) => setText(e.target.value)
+
+  const handleKeyDown = (e) => {
+    const trimmedText = e.target.value.trim()
+    // If the user pressed the Enter key:
+    if (e.key === 'Enter' && trimmedText) {
+      // Dispatch the "todo added" action with this text
+      dispatch({ type: 'todos/todoAdded', payload: trimmedText })
+      // And clear out the text input
+      setText('')
+    }
+  }
+
   return (
     <header className="header">
       <input
@@ -10,6 +25,7 @@ export default function Header() {
         placeholder="What needs to be done?"
         value={text}
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
     </header>
   )
--- src/index.js
+++ src/index.js
@@ -1,35 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import './index.css'
+
 import App from './App'
-
-import './api/server'
-
 import store from './store'
 
-console.log('Initial state: ', store.getState())
+const root = createRoot(document.getElementById('root'))
 
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about reducers' })
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about stores' })
-
-store.dispatch({ type: 'todos/todoToggled', payload: 0 })
-store.dispatch({ type: 'todos/todoToggled', payload: 1 })
-
-store.dispatch({ type: 'filters/statusFilterChanged', payload: 'Active' })
-
-store.dispatch({
-  type: 'filters/colorFilterChanged',
-  payload: { color: 'red', changeType: 'added' },
-})
-
-// Dispatch one more action to see what happens
-
-store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
-
-ReactDOM.render(
+root.render(
+  // Render a `<Provider>` around the entire `<App>`,
+  // and pass the Redux store to it as a prop
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
